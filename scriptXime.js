e6133e9a9fe226179cefce93314e6440fa1a0017
Christian Acevedo <christian_acevedo@hotmail.com>
2020-06-13 16:55:54 -0500

    Primer commit

---
+++ scriptXime.js
@@ -0,0 +1,13 @@
+function run() {
+
+    swal({
+
+        title: "Tu compra ha sido realizada con exito!",
+
+        text: "You clicked the button!",
+
+        icon: "success",
+
+        button: "OK baby!",
+
+      })};
